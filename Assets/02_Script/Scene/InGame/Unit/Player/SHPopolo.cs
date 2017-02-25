5009b1ac2bbc5b5abaf35ccd200f485935630dc9
shmhlove <shmhlove@naver.com>
2017-02-25 16:25:03 +0900

    [클라] 캐릭터 파츠조립, 애니메이션 3종, (임시) 애니메이션 컨트롤러 구현

--- Assets/02_Script/Scene/InGame/Unit/Player/SHPopolo.cs
+++ Assets/02_Script/Scene/InGame/Unit/Player/SHPopolo.cs
@@ -16,6 +16,7 @@ public class SHPopolo : SHState
     #region Members : Inspector
     [Header("Player Info")]
     [SerializeField] private SHMonoWrapper m_pShootPos  = null;
+    [SerializeField] private Animation     m_pAnimation = null;
     [SerializeField] private float         m_fMoveSpeed = 50.0f;
     #endregion
 
@@ -29,6 +30,11 @@ public class SHPopolo : SHState
         SHHard.m_fPlayerMoveSpeed = SHPlayerPrefs.GetFloat("Player_MoveSpeed", m_fMoveSpeed);
 #endif
     }
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        DeConnectController();
+    }
     public override void FixedUpdate()
     {
         base.FrameMove();
@@ -36,15 +42,18 @@ public class SHPopolo : SHState
     public override void RegisterState()
     {
         var pState = CreateState(eState.Idle);
-        pState.m_OnEnterState  = OnEnterStateToIdle;
-        pState.m_OnExitState   = OnExitStateToIdle;
-        pState.m_OnFixedUpdate = OnFixedUpdateToIdle;
+        {
+            pState.m_OnEnterState  = OnEnterStateToIdle;
+            pState.m_OnExitState   = OnExitStateToIdle;
+            pState.m_OnFixedUpdate = OnFixedUpdateToIdle;
+        }
+        ConnectController();
         ChangeState(eState.Idle);
     }
-#endregion
+    #endregion
 
 
-#region Utility Functions
+    #region Utility Functions
     SHStateInfo CreateState(eState eType)
     {
         return base.CreateState((int)eType);
@@ -53,43 +62,78 @@ public class SHPopolo : SHState
     {
         base.ChangeState((int)eType);
     }
-#endregion
+    void ChangeAnimation(string strClipName)
+    {
+        if (null == m_pAnimation)
+            return;
 
-
-#region State Functions
-    void OnEnterStateToIdle(int iBeforeState, int iCurrentState)
+        m_pAnimation.Play(strClipName);
+    }
+    void ConnectController()
     {
         var pCtrlUI = Single.UI.GetPanel<SHUIPanel_CtrlPad>("Panel_CtrlPad");
         if (null == pCtrlUI)
             return;
 
-        pCtrlUI.AddEventToMove((vDirection) => 
+        pCtrlUI.AddEventToMove((vDirection) =>
         {
+            if ((false == m_pAnimation.IsPlaying("Anim_Char_Attack")) &&
+                (false == m_pAnimation.IsPlaying("Anim_Char_Move")))
+                ChangeAnimation("Anim_Char_Move");
+
             AddLocalPositionX(SHHard.m_fPlayerMoveSpeed * vDirection.x);
             AddLocalPositionZ(SHHard.m_fPlayerMoveSpeed * vDirection.y);
         });
-        pCtrlUI.AddEventToDirection((vDirection) => 
+        pCtrlUI.AddEventToDirection((vDirection) =>
         {
             SetLocalRotateY(SHMath.GetAngleToPosition(Vector3.forward, -1.0f, Vector3.up, vDirection));
         });
         pCtrlUI.AddEventToShoot(() =>
         {
+            ChangeAnimation("Anim_Char_Attack");
             SH3DRoot.PlayCameraShake();
             Single.Damage.AddDamage("Dmg_Bullet",
                             new SHAddDamageParam(m_pShootPos, null, null, null));
         });
     }
-    void OnExitStateToIdle(int iBeforeState, int iCurrentState)
+    void DeConnectController()
     {
+        if (false == SHUIManager.IsExists)
+            return;
+
         var pCtrlUI = Single.UI.GetPanel<SHUIPanel_CtrlPad>("Panel_CtrlPad");
         if (null == pCtrlUI)
             return;
 
         pCtrlUI.Clear();
     }
+    #endregion
+
+
+    #region State Functions
+    // 대기상태
+    void OnEnterStateToIdle(int iBeforeState, int iCurrentState)
+    {
+    }
+    void OnExitStateToIdle(int iBeforeState, int iCurrentState)
+    {
+    }
     void OnFixedUpdateToIdle(int iCurrentState, int iFixedTick)
     {
-
+        if ((false == m_pAnimation.IsPlaying("Anim_Char_Attack")) &&
+            (false == m_pAnimation.IsPlaying("Anim_Char_Move")) &&
+            (false == m_pAnimation.IsPlaying("Anim_Char_Wait")))
+                ChangeAnimation("Anim_Char_Wait");
     }
-#endregion
+
+    // 이동상태
+    //void OnEnterStateToMove(int iBeforeState, int iCurrentState) { }
+    //void OnExitStateToMove(int iBeforeState, int iCurrentState) { }
+    //void OnFixedUpdateToMove(int iCurrentState, int iFixedTick) { }
+
+    // 공격상태
+    //void OnEnterStateToAttack(int iBeforeState, int iCurrentState) { }
+    //void OnExitStateToAttack(int iBeforeState, int iCurrentState) { }
+    //void OnFixedUpdateToAttack(int iCurrentState, int iFixedTick) { }
+    #endregion
 }
