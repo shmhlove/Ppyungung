65edbcdc1c96fa426440e7f65694e1eca60fa44c
이상호 <shmhlove@naver.com>
2017-03-04 19:36:21 +0900

    [클라] 몬스터 이속에도 카메라 속도 보태주기

--- Assets/02_Script/Scene/InGame/Unit/Monster/Mouse/SHMonMouse_Utility.cs
+++ Assets/02_Script/Scene/InGame/Unit/Monster/Mouse/SHMonMouse_Utility.cs
@@ -25,8 +25,12 @@ public partial class SHMonMouse : SHState
     }
     void SetMove()
     {
-        AddLocalPositionX(SHHard.m_fMonMoveSpeed * m_vDirection.x);
-        AddLocalPositionZ(SHHard.m_fMonMoveSpeed * m_vDirection.z);
+        AddLocalPositionX((SHHard.m_fMonMoveSpeed + SHHard.m_fCameraMoveSpeed) * m_vDirection.x);
+
+        if (0.0f < m_vDirection.z)
+            AddLocalPositionZ((SHHard.m_fMonMoveSpeed + SHHard.m_fCameraMoveSpeed) * m_vDirection.z);
+        else
+            AddLocalPositionZ(SHHard.m_fMonMoveSpeed * m_vDirection.z);
     }
     #endregion
 
