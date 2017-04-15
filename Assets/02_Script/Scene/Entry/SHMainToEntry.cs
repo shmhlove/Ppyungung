b60980f874211a6cd83d5dd7be1a6e95d0c5d8d1
이상호 <shmhlove@naver.com>
2017-04-16 06:32:32 +0900

    [클라] 카메라 Shake 애니메이션 제거

--- Assets/02_Script/Scene/Entry/SHMainToEntry.cs
+++ Assets/02_Script/Scene/Entry/SHMainToEntry.cs
@@ -6,11 +6,6 @@ public class SHMainToEntry : SHMonoWrapper
 {
     public override void Start() 
     {
-        if (true == Single.AppInfo.IsDevelopment())
-            Single.UI.Show("Panel_Development");
-        else
-            Single.UI.Close("Panel_Development");
-
         Single.Scene.GoTo(eSceneType.InGame);
     }
 }
