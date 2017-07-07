ca440863e156fe5dbf04eeb85b270bd87c07dc37
이상호 <shmhlove@ncsoft.com>
2017-07-07 10:19:20 +0900

    [테스트 종료]  체리픽

--- Assets/02_Script/Utility/SHCoroutine.cs
+++ Assets/02_Script/Utility/SHCoroutine.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using UnityEngine;
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class SHCoroutine : SHSingleton<SHCoroutine>
 {
